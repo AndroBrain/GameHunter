22a72d5f575f73f486ab61a575781387c4ac7aa5
Michal Ankiersztajn <michalankiersztajn@gmail.com>
2024-01-12 14:34:27 +0100

    Distinct models by gameId

--- composeApp/src/commonMain/kotlin/domain/deal/GetDealsUseCase.kt
+++ composeApp/src/commonMain/kotlin/domain/deal/GetDealsUseCase.kt
@@ -4,4 +4,5 @@ class GetDealsUseCase(
     private val dealRepository: DealRepository,
 ) {
     suspend operator fun invoke(): List<DealModel> = dealRepository.getDeals()
+        .distinctBy { model -> model.gameID }
 }
