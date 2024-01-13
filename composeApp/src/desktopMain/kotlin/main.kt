4655ad9a0a2fcdaea558b6bc26b31f35c390bcf7
Michal Ankiersztajn <michalankiersztajn@gmail.com>
2024-01-13 20:41:27 +0100

    Add Desktop App Icon

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,4 +1,3 @@
-
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
@@ -10,7 +9,10 @@ import app.ui.screen.root.RootComponent
 import com.arkivanov.decompose.DefaultComponentContext
 import com.arkivanov.decompose.extensions.compose.jetbrains.lifecycle.LifecycleController
 import com.arkivanov.essenty.lifecycle.LifecycleRegistry
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.painterResource
 
+@OptIn(ExperimentalResourceApi::class)
 fun main() {
     val lifecycle = LifecycleRegistry()
 
@@ -26,7 +28,11 @@ fun main() {
 
         LifecycleController(lifecycle, windowState)
 
-        Window(onCloseRequest = ::exitApplication, title = "GameHunter") {
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = "GameHunter",
+            icon = painterResource("app-icon.xml"),
+        ) {
             App(root = root)
         }
     }
