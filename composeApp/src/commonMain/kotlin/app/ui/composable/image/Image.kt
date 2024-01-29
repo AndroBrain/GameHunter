68b9e5f62550f9c3d32fe20dc5b458285a443b6c
Michal Ankiersztajn <michalankiersztajn@gmail.com>
2024-01-29 19:04:13 +0100

    Add cacheControl to AsyncImage

--- composeApp/src/commonMain/kotlin/app/ui/composable/image/Image.kt
+++ composeApp/src/commonMain/kotlin/app/ui/composable/image/Image.kt
@@ -4,9 +4,13 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import io.kamel.core.Resource
+import io.kamel.core.utils.cacheControl
 import io.kamel.image.KamelImage
 import io.kamel.image.asyncPainterResource
+import io.ktor.client.utils.CacheControl
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
 import kotlinx.coroutines.Job
@@ -18,9 +22,15 @@ fun AsyncImage(
     contentDescription: String? = null,
     contentScale: ContentScale = ContentScale.Crop,
 ) {
+    val resource: Resource<Painter> = asyncPainterResource(url) {
+        coroutineContext = Job() + Dispatchers.IO
+        requestBuilder {
+            cacheControl(CacheControl.MAX_AGE)
+        }
+    }
     KamelImage(
         modifier = modifier,
-        resource = asyncPainterResource(url) { coroutineContext = Job() + Dispatchers.IO },
+        resource = resource,
         contentDescription = contentDescription,
         onLoading = { CircularProgressIndicator(modifier = Modifier.align(Alignment.Center)) },
         contentScale = contentScale,
