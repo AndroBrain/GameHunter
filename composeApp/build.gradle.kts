ae793aec482f05c88922fd3d74654f3617dfdfcd
Michal Ankiersztajn <michalankiersztajn@gmail.com>
2024-02-11 14:11:43 +0100

    Add proguard-rules and use it in Android build

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -107,7 +107,12 @@ android {
     }
     buildTypes {
         getByName("release") {
-            isMinifyEnabled = false
+            isShrinkResources = true
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     compileOptions {
